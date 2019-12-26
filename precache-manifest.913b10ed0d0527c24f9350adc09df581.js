234dc0c8d6ccaa919b5f166eeb61d14f6b0f0cc2
Suzette McCanny <suzettemccanny@gmail.com>
2019-12-26 08:44:07 -0500

    Updates

---
+++ precache-manifest.913b10ed0d0527c24f9350adc09df581.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41763b304816f4f970d647846968a4ec",
+    "url": "/ediblePlants/index.html"
+  },
+  {
+    "revision": "8567818aeb367bd766f0",
+    "url": "/ediblePlants/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "fd931a0106ca9098d194",
+    "url": "/ediblePlants/static/css/main.71209bda.chunk.css"
+  },
+  {
+    "revision": "8567818aeb367bd766f0",
+    "url": "/ediblePlants/static/js/2.de6feca7.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/ediblePlants/static/js/2.de6feca7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd931a0106ca9098d194",
+    "url": "/ediblePlants/static/js/main.642b7ea7.chunk.js"
+  },
+  {
+    "revision": "15f95216d73eb9bfd604",
+    "url": "/ediblePlants/static/js/runtime-main.c5189860.js"
+  },
+  {
+    "revision": "f212b86fe25352f57593f33836bd4af8",
+    "url": "/ediblePlants/static/media/plant2.f212b86f.svg"
+  }
+]);
